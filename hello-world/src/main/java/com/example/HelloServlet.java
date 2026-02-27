dd58961318e5e68af324f665af88cf592febdc69
Ilya Vozvyshaev <ilya.vozvyshaev@gmail.com>
2026-02-27 21:05:14 +0300

    Fix bag

--- hello-world/src/main/java/com/example/HelloServlet.java
+++ hello-world/src/main/java/com/example/HelloServlet.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet{
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/pain");
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
         PrintWriter out = response.getWriter();
         out.print("Hello world");
     }
